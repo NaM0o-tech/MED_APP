90bb0a5e2711fa129eee4dbb291df9d70c9b23a0
Namo <namo190611@gmail.com>
2026-09-12 22:50:43 +0700

    b

--- src/app/account.tsx
+++ src/app/account.tsx
@@ -1,200 +1,312 @@
-import { router } from 'expo-router';
-//import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router, useLocalSearchParams } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function account() {
+  const [name, Setname] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+
+  let welcome = "เข้าใช้งาน";
+
+  const { mode } = useLocalSearchParams();
+  if (mode === "edit") {
+    welcome = "แก้ไขข้อมูล";
+  }
+
+  const isFormComplete =
+    name.trim() !== "" && age.trim() !== "" && gender !== "";
+
+  const clearProfile = async () => {
+    await AsyncStorage.removeItem("userProfile");
+    Setname("");
+    setAge("");
+    setGender("");
+  };
+
+  useEffect(() => {
+    //clearProfile();
+    loadprofile();
+    router.prefetch("/main");
+  }, []);
+
+  const loadprofile = async () => {
+    const saved = await AsyncStorage.getItem("userProfile");
+    if (saved) {
+      const profile = JSON.parse(saved);
+      Setname(profile.name);
+      setAge(profile.age);
+      setGender(profile.gender);
+
+      if (mode !== "edit") {
+        router.push("/main");
+      }
+    }
+  };
+
+  const saved_profile = async () => {
+    // เช็คว่ากรอกชื่อหรือยัง
+    if (name.trim() === "") {
+      alert("กรุณากรอกชื่อ");
+      return;
+    }
+
+    if (name.length >= 15) {
+      alert("กรุณากรอกชื่อไม่เกิน 15 ตัวอักษร");
+      return;
+    }
+
+    // เช็คว่ากรอกอายุหรือยัง และต้องเป็นตัวเลข
+    if (age.trim() === "" || isNaN(Number(age))) {
+      alert("กรุณากรอกอายุเป็นตัวเลข");
+      return;
+    }
+
+    // เช็คว่าเลือกเพศหรือยัง
+    if (gender === "") {
+      alert("กรุณาเลือกเพศ");
+      return;
+    }
+
+    // เช็คช่วงอายุที่สมเหตุสมผล (ถ้าอยากเข้มงวดขึ้น)
+    if (Number(age) <= 0 || Number(age) > 120) {
+      alert("กรุณากรอกอายุที่ถูกต้อง");
+      return;
+    }
+
+    const profile = { name, age, gender };
+    await AsyncStorage.setItem("userProfile", JSON.stringify(profile));
+    router.push("/main");
+
+    alert("บันทึกข้อมูลเรียบร้อยแล้ว");
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-
-      <StatusBar
-        style="dark"
-        
-      />
+      <StatusBar style="dark" />
 
       <Text style={styles.title}>TIME MED</Text>
 
-      <Image style={styles.ppkimg} source={require('../../assets/expo.icon/Assets/ppk.png')} />
-
       <View style={styles.welcome}>
-            <Text style={styles.textwelcome}>เข้าใช้งาน</Text>
+        <Text style={styles.textwelcome}>{welcome}</Text>
       </View>
 
-        <View style={styles.card}>
-            <Text style={styles.des}>กรุณาเข้าสู่ระบบ หรือสมัครบัญชี</Text>
+      <View style={styles.card}>
+        <Text style={styles.des}>กรุณากรอกข้อมูลส่วนตัว</Text>
 
-            <TouchableOpacity style={styles.google} activeOpacity={0.6}>
-                <Text style={styles.googletext}>เข้าสู่ระบบ google</Text>
-            </TouchableOpacity>
+        <Image
+          style={styles.ppkimg}
+          source={require("../../assets/images/ppk.png")}
+        />
 
-            <TouchableOpacity style={styles.facebook} activeOpacity={0.6}>
-                <Text style={styles.facebooktext}>เข้าสู่ระบบ Facebook</Text>
-            </TouchableOpacity>
+        <Text style={styles.label}>ชื่อ</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={Setname}
+          placeholder="กรอกชื่อเล่น"
+        />
 
-            <TouchableOpacity style={styles.line} activeOpacity={0.6}>
-                <Text style={styles.linetext}>เข้าสู่ระบบ Line</Text>
-            </TouchableOpacity>
+        <Text style={styles.label}>อายุ</Text>
+        <TextInput
+          style={styles.input}
+          value={age}
+          onChangeText={setAge}
+          placeholder="กรอกอายุ"
+          keyboardType="numeric"
+        />
 
-            <TouchableOpacity style={styles.skip} activeOpacity={0.4} onPress={() => router.push('./main') }>
-                <Text style={styles.skiptext}>เข้าแอปในฐานะ guest</Text>
-            </TouchableOpacity>
+        <Text style={styles.label}>เพศ</Text>
+        <View style={styles.genderRow}>
+          <TouchableOpacity
+            style={[
+              styles.genderButton,
+              gender === "male" && styles.genderSelected,
+            ]}
+            onPress={() => setGender("male")}
+          >
+            <Text>ชาย</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.genderButton,
+              gender === "female" && styles.genderSelected,
+            ]}
+            onPress={() => setGender("female")}
+          >
+            <Text>หญิง</Text>
+          </TouchableOpacity>
         </View>
 
+        <TouchableOpacity
+          style={[
+            styles.savedbutton,
+            isFormComplete && styles.savedbuttonActive,
+          ]}
+          activeOpacity={0.4}
+          onPress={saved_profile}
+        >
+          <Text
+            style={[styles.savedtext, isFormComplete && styles.savetextActive]}
+          >
+            บันทึกข้อมูล
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     flex: 1,
-    backgroundColor: '#e3efff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#e3efff",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    alignItems: "center", // การ์ดยังอยู่กึ่งกลางแนวนอนของจอ
     padding: 20,
-    },
-    title: {
-    fontSize: 71,
-    fontWeight: 'bold',
-    color: '#f6c8ff',
-    marginBottom: 0,
-    textAlign: 'center',
-    position: 'absolute',
-    top: 80,
-    textShadowColor: '#000000',
+    paddingTop: 60,
+  },
+  title: {
+    fontSize: 60,
+    fontWeight: "bold",
+    color: "#f6c8ff",
+    textAlign: "center",
+    marginBottom: 25,
+    textShadowColor: "#000000",
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 1,
   },
+  textwelcome: {
+    fontSize: 20,
+    fontWeight: "800",
+  },
+  ppkimg: {
+    width: 70,
+    height: 70,
+    position: "absolute",
+    top: 14,
+    right: 12,
+    zIndex: 15,
+    elevation: 50,
+    shadowColor: "#ff0bb6",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    transform: [{ rotate: "8deg" }],
+  },
+  welcome: {
+    backgroundColor: "#f6daff",
+    borderRadius: 10,
+    borderWidth: 0.8,
+    borderColor: "black",
+    width: "45%",
+    paddingVertical: 8,
+    alignItems: "center",
+    justifyContent: "center",
+
+    position: "absolute",
+    top: 195,
+    left: 40,
+    zIndex: 10,
+  },
   card: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     padding: 20,
     borderRadius: 16,
-    width: '88%',
-    height: '60%', 
-    alignItems: 'center',
-    justifyContent: 'center',
-    // Shadow สำหรับ iOS/Android
+    width: "88%",
     elevation: 12,
-    shadowColor: '#ff0bb6',
+    shadowColor: "#ff0bb6",
     shadowOffset: { width: 0, height: 5 },
     shadowOpacity: 1,
     shadowRadius: 8,
     borderWidth: 0.5,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    textAlign: 'center',
-    zIndex: 1,
-    position: 'absolute',
-    bottom: 130,
-  },
-  welcome: {
-    position: 'absolute',
-    backgroundColor: '#f6daff',
-    borderRadius: 10,
-    borderWidth: 0.8,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    textAlign: 'center',
-    width: '45%',
-    paddingVertical: 8,
-    top: 225,
+    borderColor: "black",
 
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 10,
-  },
-
-  textwelcome: {
-    fontSize: 20,
-    fontWeight: '800',
-  },
-  ppkimg: {
-    width: 100,
-    height: 100,
-    position: 'absolute',
-    top: 190,
-    right: 5,
-    zIndex: 15,
-    elevation: 50,
-    shadowColor: '#ff0bb6',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 1,
-    shadowRadius: 10,
-    transform: [{ rotate: '8deg' }]
+    position: "absolute",
+    top: "30%",
+    left: "6%",
   },
   des: {
-    position: 'absolute',
-    top: 50,
-    color: '#8d8d8d'
+    color: "#8d8d8d",
+    fontSize: 16,
+    marginTop: 15,
+    marginBottom: 5,
+    marginLeft: 45,
+    fontWeight: "600",
   },
-  skip: {
-    position: 'absolute',
-    backgroundColor: '#c5c4c4',
+  label: {
+    fontSize: 14,
+    marginBottom: 4,
+    marginTop: 12,
+    alignSelf: "flex-start", // label ชิดซ้าย
+  },
+  input: {
+    width: "100%", // input เต็มความกว้างการ์ด
     borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#a3a3a3',
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 8,
+    fontSize: 16,
+  },
+  genderRow: {
+    flexDirection: "row",
+    gap: 10,
+    width: "100%", // แถวปุ่มเพศเต็มความกว้าง
+  },
+  genderButton: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 10,
+    flex: 1,
+    alignItems: "center",
+  },
+  genderSelected: {
+    backgroundColor: "#e3efff",
+    borderColor: "#378ADD",
+  },
+  savedbutton: {
+    backgroundColor: "#c5c4c4",
+    borderWidth: 1,
+    borderColor: "#a3a3a3",
     paddingVertical: 8,
     width: 150,
     borderRadius: 50,
-    bottom: 20,
+    marginTop: 20,
+    alignSelf: "center", // ปุ่มบันทึกอยู่กึ่งกลางการ์ด (ปรับเป็น flex-start ได้ถ้าอยากชิดซ้ายด้วย)
   },
-  skiptext: {
-    color: '#555555',
-    fontWeight: '600',
-    textAlign: 'center',
-    fontSize: 10,
+  savedtext: {
+    color: "#555555",
+    fontWeight: "600",
+    textAlign: "center",
+    fontSize: 15,
   },
-  google: {
-    position: 'absolute',
-    backgroundColor: '#ffffff',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#c2c1c1',
-    paddingVertical: 8,
-    width: '80%',
-    borderRadius: 50,
-    top: 100,
-  },
-  googletext: {
-    color: '#000000',
-    fontWeight: '600',
-    textAlign: 'center',
-    fontSize: 13,
-  },
-  facebook: {
-    position: 'absolute',
-    backgroundColor: '#608aff',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#0632f3',
-    paddingVertical: 8,
-    width: '80%',
-    borderRadius: 50,
-    top: 150,
-  },
-  facebooktext: {
-    color: '#fdfbfb',
-    fontWeight: '600',
-    textAlign: 'center',
-    fontSize: 13,
-  },
-  line: {
-    position: 'absolute',
-    backgroundColor: '#7edd61',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#0d8103',
-    paddingVertical: 8,
-    width: '80%',
-    borderRadius: 50,
-    top: 200,
-  },
-  linetext: {
-    color: '#fdfbfb',
-    fontWeight: '600',
-    textAlign: 'center',
-    fontSize: 13,
-  }
 
-})
+  savedbuttonActive: {
+    backgroundColor: "#ffff95",
+    borderWidth: 1,
+    borderColor: "#000000",
+    paddingVertical: 8,
+    width: 150,
+    borderRadius: 50,
+    marginTop: 20,
+    alignSelf: "center", // ปุ่มบันทึกอยู่กึ่งกลางการ์ด (ปรับเป็น flex-start ได้ถ้าอยากชิดซ้ายด้วย)
+  },
+  savetextActive: {
+    color: "#000000",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,115 +1,325 @@
 //import { router } from 'expo-router';
-//import { useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 //import { SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function account() {
+  const [name, Setname] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [page, setpage] = useState("home");
+
+  const [time, settime] = useState(new Date());
+  const [period, setperiod] = useState("");
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  useEffect(() => {
+    const timeinterval = setInterval(() => {
+      settime(new Date());
+
+      if (time.getHours() >= 6 && time.getHours() < 12) {
+        setperiod("สวัสดีตอนเช้า 🌄");
+      } else if (time.getHours() >= 12 && time.getHours() < 18) {
+        setperiod("สวัสดีตอนบ่าย ☀️");
+      } else if (time.getHours() >= 18 && time.getHours() < 24) {
+        setperiod("สวัสดีตอนเย็น 🌙");
+      }
+    }, 1000);
+
+    return () => clearInterval(timeinterval);
+  }, []);
+
+  const loadProfile = async () => {
+    const saved = await AsyncStorage.getItem("userProfile");
+    if (saved) {
+      const profile = JSON.parse(saved);
+      Setname(profile.name);
+      setAge(profile.age);
+      setGender(profile.gender);
+      setpage(page);
+      setperiod(profile.period);
+    }
+  };
+
+  const profileImages = {
+    male: require("../../assets/images/userblue.png"),
+    female: require("../../assets/images/userpink.png"),
+  };
+
+  const modifyProfile = () => {
+    router.push("/account?mode=edit");
+  };
 
   return (
-
-    
-
     <View style={styles.container}>
-
-      <StatusBar
-        style="dark"
-        
-      />
-
+      <StatusBar style="dark" />
 
       <View style={styles.card}>
-          <Text style={styles.title}>TIME MED</Text>
-          <Image style={styles.ppkimg} source={require('../../assets/expo.icon/Assets/ppk.png')} />
-          <Text style={styles.des}>แอปพลิเคชั่นแจ้งเตือนการกินยา</Text>
+        <Text style={styles.title}>TIME MED</Text>
+        <Image
+          style={styles.ppkimg}
+          source={require("../../assets/expo.icon/Assets/ppk.png")}
+        />
+
+        <View style={styles.page}>
+          {page === "home" && (
+            <>
+              <Text style={styles.pageText}>{period}</Text>
+              <View></View>
+            </>
+          )}
+          {page === "profile" && (
+            <>
+              <Text style={styles.pageText}>หน้าโปรไฟล์ 👥</Text>
+              <View style={styles.profilecard}>
+                <Image
+                  style={styles.profilepic}
+                  source={profileImages[gender as "male" | "female"]}
+                />
+                <Text style={styles.textpro}>ข้อมูลของคุณ</Text>
+                <Text style={styles.textname}>{name}</Text>
+
+                <TouchableOpacity
+                  style={styles.modifyproButton}
+                  onPress={modifyProfile}
+                  activeOpacity={0.4}
+                >
+                  <Text style={styles.modifyproButtonText}>แก้ไขข้อมูล</Text>
+                </TouchableOpacity>
+              </View>
+            </>
+          )}
+        </View>
+
+        <View style={styles.pagechange}>
+          <TouchableOpacity
+            style={page === "home" ? styles.pagebutselected : styles.pagebut}
+            onPress={() => setpage("home")}
+          >
+            <Image
+              style={styles.pageimg}
+              source={require("../../assets/images/home.png")}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={page === "profile" ? styles.pagebutselected : styles.pagebut}
+            onPress={() => setpage("profile")}
+          >
+            <Image
+              style={styles.pageimg}
+              source={require("../../assets/images/account.png")}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-    backgroundColor: '#f4c7ff',
-    width: '100%',
-    height: '100%',
-    },
-    title: {
+  container: {
+    backgroundColor: "#f4c7ff",
+    width: "100%",
+    height: "100%",
+  },
+  title: {
     fontSize: 25,
-    fontWeight: 'bold',
-    color: '#eba0fa',
+    fontWeight: "bold",
+    color: "#eba0fa",
     marginBottom: 0,
-    textAlign: 'center',
-    position: 'absolute',
+    textAlign: "center",
+    position: "absolute",
     top: 20,
     left: 20,
-    textShadowColor: '#000000',
+    textShadowColor: "#000000",
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 1,
   },
   card: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     padding: 20,
     borderRadius: 16,
-    width: '100%',
-    height: '90%', 
-    bottom:'5%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "90%",
+    bottom: "5%",
+    alignItems: "center",
+    justifyContent: "flex-start",
     // Shadow สำหรับ iOS/Android
     elevation: 12,
-    shadowColor: '#ff0bb6',
+    shadowColor: "#ff0bb6",
     shadowOffset: { width: 5, height: 5 },
     shadowOpacity: 1,
     shadowRadius: 8,
     borderWidth: 0.5,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    textAlign: 'center',
+    borderStyle: "solid",
+    borderColor: "black",
+    textAlign: "center",
     zIndex: 1,
-    position: 'absolute',
+    position: "absolute",
   },
   welcome: {
-    position: 'absolute',
-    backgroundColor: '#f6daff',
+    position: "absolute",
+    backgroundColor: "#f6daff",
     borderRadius: 10,
     borderWidth: 0.8,
-    borderStyle: 'solid',
-    borderColor: 'black',
-    textAlign: 'center',
-    width: '45%',
+    borderStyle: "solid",
+    borderColor: "black",
+    textAlign: "center",
+    width: "45%",
     paddingVertical: 8,
     top: 225,
 
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     zIndex: 10,
   },
 
-  textwelcome: {
-    fontSize: 20,
-    fontWeight: '800',
-  },
   ppkimg: {
     width: 40,
     height: 40,
-    position: 'absolute',
+    position: "absolute",
     top: 18,
     left: 145,
     zIndex: 15,
     elevation: 50,
-    shadowColor: '#ff0bb6',
+    shadowColor: "#ff0bb6",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 1,
     shadowRadius: 10,
-    transform: [{ rotate: '0deg' }]
+    transform: [{ rotate: "0deg" }],
   },
-  des: {
-    position: 'absolute',
-    top: 60,
-    left:25,
-    fontSize: 13,
-    color: '#494949'
+  profilecard: {
+    backgroundColor: "#f8fcff",
+    padding: 30,
+    borderRadius: 20,
+    width: "95%",
+    height: "10%",
+    borderWidth: 0.5,
+    borderColor: "black",
+
+    position: "absolute",
+    top: 35,
+    left: 10,
+    zIndex: 10,
+
+    paddingBlock: 20,
+
+    justifyContent: "center",
+    alignItems: "flex-start",
+    flexDirection: "row",
+  },
+  profilepic: {
+    width: 55,
+    height: 55,
+    zIndex: 15,
+    elevation: 50,
+    shadowColor: "#292528",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+
+    alignSelf: "center",
+    position: "absolute",
+    left: 10,
+  },
+  textpro: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#0c0c0c7a",
+
+    position: "absolute",
+    left: 70,
+    top: 5,
+  },
+  textname: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#0c0c0c",
+    position: "absolute",
+    left: 70,
+    top: 25,
+  },
+  modifyproButton: {
+    backgroundColor: "#c5c4c4",
+    borderWidth: 1,
+    borderColor: "#a3a3a3",
+    paddingVertical: 0,
+    width: 80,
+    paddingBlock: 5,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 10,
+    top: 5,
+  },
+  modifyproButtonText: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#0c0c0c",
   },
 
-})
+  pagechange: {
+    backgroundColor: "#fff9ff",
+    borderWidth: 1,
+    borderColor: "#000000",
+    paddingVertical: 8,
+    width: 220,
+    height: 60,
+    borderRadius: 50,
+
+    position: "absolute",
+    bottom: 10,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+  pageimg: {
+    width: 25,
+    height: 25,
+  },
+  pagebut: {
+    width: 60,
+    height: 40,
+    borderWidth: 0.5,
+    borderColor: "#48254c",
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pagebutselected: {
+    width: 65,
+    height: 45,
+    borderWidth: 0.5,
+    borderColor: "#76267d",
+    backgroundColor: "#edc6ff",
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  page: {
+    width: "112%",
+    height: "85%",
+    borderWidth: 0,
+    borderColor: "#00000055",
+    borderRadius: 15,
+    zIndex: 5,
+    position: "absolute",
+    top: 65,
+  },
+  pageText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#0c0c0c",
+    marginLeft: 25,
+  },
+});
